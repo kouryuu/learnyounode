ae9312c989a05529f63ef27a45198320658adbd7
rreyes <antarticfreeze@gmail.com>
2016-03-09 17:08:38 -0600

    Beat JUGGLING ASYNC with JS objects

--- program.js
+++ program.js
@@ -1,17 +1,45 @@
-// HTTP COLLECT
+// JUGGLING ASYNC 
 
 var http = require('http');
-var url = process.argv[2];
-var printLines = (function(data_string){
-	console.log(data_string.length+'\n'+data_string);	
-
-	});
-http.get(url,function(response){
+var server1 = process.argv[2];
+var server2 = process.argv[3];
+var server3 = process.argv[4];
+var responses = {
+	server1_data:null,
+	server2_data:null,
+	server3_data:null,
+	no_responses:0,
+	addResponse:function(data_string,server){
+		this[server+'_data'] = data_string;
+		this['no_responses']++;
+		if(this.no_responses == 3)
+			console.log(this.server1_data+'\n'+
+				this.server2_data+'\n'+
+				this.server3_data);
+		}
+	};
+http.get(server1,function(response){
 	response.setEncoding('utf-8');
-	var data_string = "";
-	response.on("data",function(data){
+	var data_string = '';
+	response.on('data',function(data){
 		data_string += data;
 	});
-	response.on("end",function(){printLines(data_string)});
+	response.on('end',function(){responses.addResponse(data_string,'server1')});
+});
+http.get(server2,function(response){
+	response.setEncoding('utf-8');
+	var data_string = '';
+	response.on('data',function(data){
+		data_string += data;
+	});
+	response.on('end',function(){responses.addResponse(data_string,'server2')});
+});
+http.get(server3,function(response){
+	response.setEncoding('utf-8');
+	var data_string = '';
+	response.on('data',function(data){
+		data_string += data;
+	});
+	response.on('end',function(){responses.addResponse(data_string,'server3')});
 });
 

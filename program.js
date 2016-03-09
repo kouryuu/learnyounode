ab9a4ff0320b2e6289067314e47d20335f5828eb
rreyes <antarticfreeze@gmail.com>
2016-03-09 16:52:25 -0600

    HTTP COLLECT done.

--- program.js
+++ program.js
@@ -1,10 +1,17 @@
-// HTTP CLIENT 
+// HTTP COLLECT
+
 var http = require('http');
 var url = process.argv[2];
+var printLines = (function(data_string){
+	console.log(data_string.length+'\n'+data_string);	
+
+	});
 http.get(url,function(response){
 	response.setEncoding('utf-8');
+	var data_string = "";
 	response.on("data",function(data){
-		console.log(data);
+		data_string += data;
 	});
+	response.on("end",function(){printLines(data_string)});
 });
-	
+

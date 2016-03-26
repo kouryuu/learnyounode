f16a0ae8662d0257c3e1e0c7ac80e1cc59cd81cc
kouryuu <antarticfreeze@gmail.com>
2016-03-25 21:22:22 -0600

    Testing file server

--- program.js
+++ program.js
@@ -1,17 +1,20 @@
-// TIME SERVER
+// FILE SERVER
 var port = process.argv[2];
-var net = require('net');
-var server = net.createServer(function(socket){
-	var date = new Date();
-	var year = date.getFullYear();
-	var month = date.getMonth() < 10?"0"+(date.getMonth()+1):date.getMonth()+1;
-	var day = date.getDate();
-	var hour = date.getHours();
-	var min = date.getMinutes() < 10?"0"+date.getMinutes():date.getMinutes();
-	var date_string = year+"-"+month+"-"+day+" "+hour+":"+min+'\n';
-	socket.write(date_string);
-	socket.end();
+var file_name = process.argv[3];
+var fs = require('fs');
+var http = require('http');
+
+var server = http.createServer(function(request,response){
+	response.writeHead(200,{'Content-Type':'text/plain'});
+	var file = fs.createReadStream(file_name);
+	file.on('open',function(){
+		file.pipe(response);
+		response.end();
+	});
+
+
+
+
+
 });
 server.listen(port);
-
-
